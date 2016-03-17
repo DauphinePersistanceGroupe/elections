ca0616a4271e85df38a70f8e4b65d9666fcab7d8
Omar TRABELSI <omar.trabelsi.ing@gmail.com>
2016-03-17 16:13:14 +0100

    add log attribute

--- trabelsi/election/src/main/java/fr/dauphine/lamsade/hib/elections/election/App.java
+++ trabelsi/election/src/main/java/fr/dauphine/lamsade/hib/elections/election/App.java
@@ -1,26 +1,29 @@
 package fr.dauphine.lamsade.hib.elections.election;
 
-
 import java.sql.*;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
+
 //test 
 public class App {
+
+	private static Logger log = Logger.getLogger(App.class.getCanonicalName());
+
 	public static void main(String[] args) {
 
 		Driver driver = null;
 
 		try {
 
-			driver =  DriverManager.getDriver("jdbc:h2:~/test");
+			driver = DriverManager.getDriver("jdbc:h2:~/test");
 
-			System.out.println(
-					"driver pilote version number = " + " " + driver.getMajorVersion() + "." + driver.getMinorVersion());
+			log.info("driver pilote version number = " + " " + driver.getMajorVersion() + "."
+					+ driver.getMinorVersion());
 
 		} catch (SQLException e) {
 
-			System.out.println("Connection Failed! Check output console");
-			e.printStackTrace();
+			log.info("Connection Failed! Check output console");
 			return;
 
 		}
