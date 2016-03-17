4ac345a6cde9aec5b103b4d9e75081194f2da575
omar <omar>
2016-03-17 15:42:46 +0100

    test connection

---
+++ trabelsi/election/src/main/java/fr/dauphine/lamsade/hib/elections/election/App.java
@@ -0,0 +1,28 @@
+package fr.dauphine.lamsade.hib.elections.election;
+
+
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+//test 
+public class App {
+	public static void main(String[] args) {
+
+		Driver driver = null;
+
+		try {
+
+			driver =  DriverManager.getDriver("jdbc:h2:~/test");
+
+			System.out.println(
+					"driver pilote version number = " + " " + driver.getMajorVersion() + "." + driver.getMinorVersion());
+
+		} catch (SQLException e) {
+
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+
+		}
+	}
+}
