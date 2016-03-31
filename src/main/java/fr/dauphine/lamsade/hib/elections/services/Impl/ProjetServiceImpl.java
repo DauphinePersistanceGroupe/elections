cb16fc8616753ae645ca1083083cb3c64e0f8634
omar <omar>
2016-03-31 16:47:37 +0200

    update ProjetService

--- src/main/java/fr/dauphine/lamsade/hib/elections/services/Impl/ProjetServiceImpl.java
+++ src/main/java/fr/dauphine/lamsade/hib/elections/services/Impl/ProjetServiceImpl.java
@@ -3,54 +3,130 @@
  */
 package fr.dauphine.lamsade.hib.elections.services.Impl;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
+
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
 
-import fr.dauphine.lamsade.hib.elections.Exception.Exceptions;
+import fr.dauphine.lamsade.hib.elections.Exception.MyExceptions;
 import fr.dauphine.lamsade.hib.elections.domain.Projet;
 import fr.dauphine.lamsade.hib.elections.services.ProjetService;
-import fr.dauphine.lamsade.hib.elections.utils.SQLConstantes;
 
 /**
  * @author omar.trabelsi
  *
  */
-public class ProjetServiceImpl implements ProjetService{
+public class ProjetServiceImpl implements ProjetService {
+
+	@PersistenceContext(unitName = "electionsPU")
+	EntityManager em;
+
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.dauphine.lamsade.hib.elections.services.ProjetService#findById(java.
+	 * lang.Long)
+	 */
+	@Override
+	public Projet findById(Long id) throws MyExceptions {
+		try {
+			return em.find(Projet.class, id);
+		} catch (IllegalArgumentException e) {
+			throw new MyExceptions(e.getMessage(), e);
+		}
+	}
 	
-	
-	@PersistenceContext
-    private EntityManager em;
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.dauphine.lamsade.hib.elections.services.ProjetService#findByName(java
+	 * .lang.String)
+	 */
+	@Override
+	public List<Projet> findByName(String name) throws MyExceptions {
+		try {
+			Query query = em
+					.createQuery("SELECT p FROM Projet p WHERE p.nom = :name");
+			query.setParameter("name", name);
+			return  (List<Projet>) query.getSingleResult();
+		} catch (IllegalArgumentException | PersistenceException e) {
+			throw new MyExceptions(e.getMessage(), e);
+		}
+	}
 
-	public Projet findById(Long id) throws Exceptions{
-		Projet projet = em.find(Projet.class, id);
-		return projet;
-		
-	};
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see fr.dauphine.lamsade.hib.elections.services.ProjetService#findAll()
+	 */
+	@Override
+	public List<Projet> findAll() throws MyExceptions {
+		try {
+			Query query = em
+					.createQuery("SELECT p FROM Projet p ");
+			return  (List<Projet>) query.getSingleResult();
+		} catch (IllegalArgumentException | PersistenceException e) {
+			throw new MyExceptions(e.getMessage(), e);
+		}
+	}
 
-	public Projet findByName(String name) throws Exceptions{
-		Projet projet = em.find(Projet.class, name);
-		return projet;
-	};
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.dauphine.lamsade.hib.elections.services.ProjetService#create(fr.dauphine
+	 * .lamsade.hib.elections.domain.Projet)
+	 */
+	@Override
+	public void create(Projet projet) throws MyExceptions {
+		try {
+			em.persist(projet);;
+		} catch (IllegalArgumentException e) {
+			throw new MyExceptions(e.getMessage(), e);
+		}
 
-	public List<Projet> findAll() throws Exceptions{
-		Query query = em.createQuery("SELECT p FROM Projet p");
-		query.getResultList();
-		reurn null;
-	};
+	}
 
-	void create(Projet projet) throws Exceptions;
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.dauphine.lamsade.hib.elections.services.ProjetService#delete(fr.dauphine
+	 * .lamsade.hib.elections.domain.Projet)
+	 */
+	@Override
+	public void delete(Projet projet) throws MyExceptions {
+		try {
+			 em.remove(projet);
+		} catch (IllegalArgumentException e) {
+			throw new MyExceptions(e.getMessage(), e);
+		}
 
-	void delete(Projet projet) throws Exceptions;
+	}
 
-	void update(Projet projet) throws Exceptions;
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.dauphine.lamsade.hib.elections.services.ProjetService#update(fr.dauphine
+	 * .lamsade.hib.elections.domain.Projet)
+	 */
+	@Override
+	public void update(Projet projet) throws MyExceptions {
+		try {
+			 em.merge(projet);
+		} catch (IllegalArgumentException e) {
+			throw new MyExceptions(e.getMessage(), e);
+		}
+
+
+	}
 
 
 }
