0d005b509885d54aa1a73d66c5298293e2f64be7
barou <barou2000@hotmail.com>
2016-04-08 12:00:57 +0200

    web pages

--- src/main/java/fr/dauphine/lamsade/hib/elections/utils/UrlConstantes.java
+++ src/main/java/fr/dauphine/lamsade/hib/elections/utils/UrlConstantes.java
@@ -9,6 +9,11 @@ package fr.dauphine.lamsade.hib.elections.utils;
  */
 public class UrlConstantes {
 
-	public static final String ADMIN_ACCUEIL="userEdit";
+	public static final String ACCUEIL="index";
+	public static final String LOGIN="login";
+	public static final String INSCRIPTION="inscription";
+	public static final String ADMIN_ACCUEIL="admin";
+	public static final String USER_ACCUEIL="user";
 	public static final String USER_EDIT="userEdit";
+	
 }
